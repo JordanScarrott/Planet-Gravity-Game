00d8f9ea6377ee47ef9e194b7794cd34ea8388ab
CosmicNeanderthal <jackforde0@gmail.com>
2016-12-05 17:52:27 +0200

    Game Class

--- Game.java
+++ Game.java
@@ -1,22 +1,22 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
 
 public class Game extends JPanel {
-//BufferedImage img = null;
-//    try {
-//        back = ImageIO.read(new File("Images/back.jpg"));
-//        circle = ImageIO.read(new File("Images/circle.png"));
-//    } catch (IOException e) {
-//        System.out.println("OOPS");  //prints "WRONG BACKGROUND" if there is an issue obtaining the background
-//    }}
 
-    public Game(JFrame frame) {
+    ResourceLoader resLoader;
+    BufferedImage img;
 
+    public Game(JFrame frame, ResourceLoader in_resLoader) {
+
+        resLoader = in_resLoader;
+        img = resLoader.loadImage("ball2.png");
     }
+
     public void paint(Graphics g) {
         super.paint(g);
-        //g.drawImage(img, 0, 0, null);
     }
+
     public void move() {
     }
 }
