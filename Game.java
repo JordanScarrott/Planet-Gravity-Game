d2f7b5bac552656b2bd185b2a06182bf18c57f60
Jordan <jordanscarrott@gmail.com>
2016-12-05 18:11:38 +0200

    Instantiated a Planet object

--- Game.java
+++ Game.java
@@ -5,16 +5,19 @@ import java.awt.image.BufferedImage;
 public class Game extends JPanel {
 
     ResourceLoader resLoader;
+    Planet planet;
     BufferedImage img;
 
     public Game(JFrame frame, ResourceLoader in_resLoader) {
 
         resLoader = in_resLoader;
-        img = resLoader.loadImage("ball2.png");
+        //img = resLoader.loadImage("ball2.png");
+        planet = new Planet(300, 200, 100, in_resLoader.loadImage("circle.png"));
     }
 
     public void paint(Graphics g) {
         super.paint(g);
+        planet.paint(g);
     }
 
     public void move() {
