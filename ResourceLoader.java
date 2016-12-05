dc0cb6ca6e1f58c900d6032c6825b590d12dd0e7
CosmicNeanderthal <jackforde0@gmail.com>
2016-12-05 18:25:01 +0200

    Made Class abstract

--- ResourceLoader.java
+++ ResourceLoader.java
@@ -1,11 +1,12 @@
 import javax.imageio.ImageIO;
+import javax.sound.sampled.*;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.InputStream;
 
-public class ResourceLoader {
+public abstract class ResourceLoader {
 
-    protected BufferedImage loadImage(String file) {
+    protected static BufferedImage loadImage(String file) {
 
         ClassLoader loader = Thread.currentThread().getContextClassLoader();
         InputStream filePath = loader.getResourceAsStream("res/images/" + file);
@@ -17,5 +18,19 @@ public class ResourceLoader {
             e.printStackTrace();
         }
         return img;
+    }
+
+    protected static void loadAudio(String file) {
+
+        ClassLoader loader = Thread.currentThread().getContextClassLoader();
+        try {
+            Clip clip = AudioSystem.getClip();
+            AudioInputStream inputStream = AudioSystem.getAudioInputStream(loader.getResourceAsStream("res/sound/" + file));
+            clip.open(inputStream);
+            clip.start();
+        }
+        catch (IOException | UnsupportedAudioFileException | LineUnavailableException exx) {
+            exx.printStackTrace();
+        }
     }
 }
