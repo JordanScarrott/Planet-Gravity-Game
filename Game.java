50aa4aee618c42ddd495253868961b18f3f2dccc
CosmicNeanderthal <jackforde0@gmail.com>
2016-12-06 14:33:26 +0200

    Game

--- Game.java
+++ Game.java
@@ -1,41 +1,43 @@
 import javax.swing.*;
 import java.awt.*;
-import java.util.ArrayList;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.image.BufferedImage;
 
-public class Game extends JPanel {
+public class Game extends JPanel implements KeyListener {
 
-    ArrayList<Planet> planets = new ArrayList<Planet>();
-    private Player player;
-    private int planetID;
+    ResourceLoader resLoader;
+    BufferedImage img;
 
     public Game(JFrame frame) {
-        planetID = 0;
-        planets.add(new Planet(300, 200, 100, ResourceLoader.loadImage("orangeplanet.png")));
-        planets.add(new Planet(0, 0, 100, ResourceLoader.loadImage("orangeplanet.png")));
-        planets.add(new Planet(500, 0, 100, ResourceLoader.loadImage("orangeplanet.png")));
-        player = new Player(300, 200, 100, ResourceLoader.loadImage("hamster.png"));
-        player.addKeyListener(frame);
+        frame.addKeyListener(this);
+        img = resLoader.loadImage("circle.png");
     }
 
     public void paint(Graphics g) {
         super.paint(g);
-        for(Planet i : planets){
-            i.paint(g);
-        }
-        player.paint(g);
+        g.drawImage(img, 75, 45, null);
     }
 
     public void move() {
-        player.move();
-        if(player.getJumping()){
-            for(int i= 0; i < planets.size(); i++) {
-                if(i != planetID)
-                    if(player.checkCollision(planets.get(i).getpLocation().x, planets.get(i).getpLocation().y)){
-                        planetID = i;
-                        break;
-                    }
 
-            }
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+        if (e.getKeyCode() == KeyEvent.VK_ESCAPE) {
+            Main.gameState = 0;
         }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
     }
 }
