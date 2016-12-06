fb28c4f3756b8574ce5a18a76b0c6b110919e845
Jordan <jordanscarrott@gmail.com>
2016-12-06 15:10:14 +0200

    Added randomImage method
    Can now choose a random Imagefrom a specified directory and return it as a BufferedImage

--- ResourceLoader.java
+++ ResourceLoader.java
@@ -3,6 +3,8 @@ import javax.sound.sampled.*;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.File;
+import java.util.Random;
 
 public abstract class ResourceLoader {
 
@@ -10,6 +12,33 @@ public abstract class ResourceLoader {
 
         ClassLoader loader = Thread.currentThread().getContextClassLoader();
         InputStream filePath = loader.getResourceAsStream("res/images/" + file);
+        BufferedImage img = null;
+        try {
+            img = ImageIO.read(filePath);
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        return img;
+    }
+
+    /**
+    * Gets a random image from the specified folder
+    * @return null if the folderPath specified is not a folder
+    *   else returns a random BufferedImage from the folder
+    */
+    protected static BufferedImage getRandomImage(String folderPath) {
+        File folderToSearch = new File(folderPath);
+        System.out.println(folderToSearch.getAbsolutePath());
+
+        if (!folderToSearch.isDirectory()) return null;
+
+        Random rand = new Random();
+        int randomFileNumber = rand.nextInt(folderToSearch.listFiles().length);
+        System.out.println(randomFileNumber);
+        
+        ClassLoader loader = Thread.currentThread().getContextClassLoader();
+        InputStream filePath = loader.getResourceAsStream(folderToSearch.listFiles()[randomFileNumber].getPath());
         BufferedImage img = null;
         try {
             img = ImageIO.read(filePath);
