ef435374c27be50c33ecbc43454588f9b039ae32
Jordan <jordanscarrott@gmail.com>
2016-12-06 14:40:05 +0200

    Made this class - partially working
    The generate planets method doesn't work yet because the ResourceLoader class needs a working randomImage() method

---
+++ PlanetGenerator.java
@@ -0,0 +1,45 @@
+
+import java.util.Random;
+import java.util.ArrayList;
+
+public class PlanetGenerator {
+
+    /**
+    * Planet limiting parameters
+    */
+    public static final int MIN_RADIUS = 50;
+    public static final int MAX_RADIUS = 250;
+
+    private ArrayList<Planet> planets;
+
+    private Random rand;
+    private int numOfPlanets;
+
+    private int x, y;
+
+    // Constructor
+    /**
+    * @param frameX the width of the current JFrame
+    * @param frameY the height of the current JFrame
+    */
+    public PlanetGenerator(int frameX, int frameY) {
+        this.x = frameX;
+        this.x = frameY;
+        planets = new ArrayList<>();
+        rand = new Random();
+        numOfPlanets = 5;
+    }
+
+
+    /*
+    *
+    **/
+    public void generatePlanets() {
+        int tempRadius;
+        for(int i = 0; i < numOfPlanets; i++) {
+            tempRadius = rand.nextInt(MIN_RADIUS, MAX_RADIUS);
+            planets.add(new Planet(MyVector.randomMyVector(), tempRadius, ResourceLoader.randomImage()));
+        }
+    }
+
+}
