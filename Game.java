7829e760d87f9e12f227f2012781bc5d068a0907
FlamingSeahorse <flamingseahorsey@gmail.com>
2016-12-06 13:07:21 +0200

    Player can jump to another planet

--- Game.java
+++ Game.java
@@ -5,22 +5,27 @@ import java.awt.image.BufferedImage;
 public class Game extends JPanel {
 
     private Planet planet;
+    private Planet planet2;
     private Player player;
     private BufferedImage img;
 
     public Game(JFrame frame) {
-        planet = new Planet(300, 200, 100, ResourceLoader.loadImage("circle.png"));
-        player = new Player(300, 200, 100, ResourceLoader.loadImage("ball2.png"));
+        planet = new Planet(300, 200, 100, ResourceLoader.loadImage("orangeplanet.png"));
+        planet2 = new Planet(0, 0, 100, ResourceLoader.loadImage("circle.png"));
+        player = new Player(300, 200, 100, ResourceLoader.loadImage("char.png"));
         player.addKeyListener(frame);
     }
 
     public void paint(Graphics g) {
         super.paint(g);
         planet.paint(g);
+        planet2.paint(g);
         player.paint(g);
     }
 
     public void move() {
         player.move();
+        if(player.getJumping()){
+        }
     }
 }
--- Player.java
+++ Player.java
@@ -9,12 +9,13 @@ public class Player extends JPanel implements KeyListener {
     private BufferedImage imgPlayer = null;
     private MyVector pLocation;
     private MyVector center;
-    private int height;
+    private float height;
     private int velocity;
     private int acceleration;
     private int radius;
-    private int angle;
+    private int angle = 135;
     private boolean moving = false;
+    private boolean jumping = false;
 
     public Player(MyVector location, int radius, BufferedImage imgPlayer) {
         this.pLocation = location;
@@ -25,18 +26,40 @@ public class Player extends JPanel implements KeyListener {
     public Player(float x, float y, int radius, BufferedImage imgPlayer) {
         this(new MyVector(x, y), radius, imgPlayer);
     }
-
+    //Getters and Setters
+    public boolean getJumping() {
+        return jumping;
+    }
+    //Functions
     public void paint(Graphics g) {
         super.paint(g);
         Graphics2D g2d = (Graphics2D) g;
         AffineTransform transform = new AffineTransform();
-        transform.translate(pLocation.x, pLocation.y);
-        transform.rotate(Math.toRadians(angle), radius, radius);
+        transform.translate(pLocation.x - height, pLocation.y - height);
+        transform.rotate(Math.toRadians(angle), radius + height , radius + height) ;
         g2d.drawImage(imgPlayer, transform, this);
     }
-
     public void move() {
         if (moving) angle += 4;
+        if(jumping){
+            height+=4;
+            checkCollision();
+        }
+    }
+    public void checkCollision(){
+        if(MyVector.distanceSq(new MyVector((float)(400 + (100 + 42 + height)*Math.cos(Math.toRadians(angle - 135))),(float) (300 + (100 + 42 + height)*Math.sin(Math.toRadians(angle - 135)))), new MyVector(100, 100))<121*121){
+            land();
+
+        }
+    }
+    public void land(){
+        angle = (int) (Math.atan2((int) (300 + (100 + 42 + height)*Math.sin(Math.toRadians(angle - 135)))-100, (int) (400 + (100 + 42 + height)*Math.cos(Math.toRadians(angle - 135)))-100)*180/3.14596) + 135;
+        jumping = false;
+        pLocation.x = 0;
+        pLocation.y = 0;
+        height = 0;
+        moving = true;
+
     }
 
     public void addKeyListener(JFrame frame) {
@@ -51,17 +74,25 @@ public class Player extends JPanel implements KeyListener {
     @Override
     public void keyPressed(KeyEvent e) {
         int keyCode = e.getKeyCode();
-        if (keyCode == KeyEvent.VK_A) {
-            moving = true;
+        if(!jumping) {
+            if (keyCode == KeyEvent.VK_A) {
+                moving = true;
+            }
+        }
+        if (keyCode == KeyEvent.VK_J) {
+            moving = false;
+            jumping = true;
         }
     }
-
     @Override
     public void keyReleased(KeyEvent e) {
-        int keyCode = e.getKeyCode();
-        if (keyCode == KeyEvent.VK_A) {
-            moving = false;
-        }
     }
 
+    public MyVector getpLocation() {
+        return pLocation;
+    }
+
+    public void setpLocation(MyVector pLocation) {
+        this.pLocation = pLocation;
+    }
 }
