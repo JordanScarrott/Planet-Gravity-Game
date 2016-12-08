3b040c61b7b3c1a36cd32e564b98f4696093c238
Jordan <jordanscarrott@gmail.com>
2016-12-08 11:17:58 +0200

    Adding support for new acceleration algorithm
    Passing Planet object to the Player class instead of lots of separate variables

--- Game.java
+++ Game.java
@@ -6,8 +6,8 @@ import java.util.ArrayList;
 
 public class Game extends JPanel {
     private BufferedImage imgBackground;
-    ArrayList<Planet> planets = new ArrayList<Planet>();
-    ArrayList<Player> players = new ArrayList<Player>();
+    ArrayList<Planet> planets = new ArrayList<>();
+    ArrayList<Player> players = new ArrayList<>();
     private int[] planetID = new int[4]; //Indicates on which planet the respective player is on
     private int[][] keys = new int[4][2]; //Keyboard inputs for player 1
     private int playerbounds = 20; //Used for player collision
@@ -36,7 +36,7 @@ public class Game extends JPanel {
             if(players.get(i).getJumping()){
                 for(int j= 0; j < planets.size(); j++) {
                     if(j != planetID[i])
-                        if(players.get(i).checkCollision(planets.get(j).getpLocation().x, planets.get(j).getpLocation().y, planets.get(j).getRadius(),planets.get(j).getBbounds())){
+                        if(players.get(i).checkCollision(planets.get(i))){
                             planetID[i] = j;
                             break;
                         }
@@ -45,19 +45,15 @@ public class Game extends JPanel {
         }
         collision(); //Check player Collisions
     }
-    public boolean getFinished(){
-        return finished;
-    }
-    public void setFinished(boolean finished){
-        this.finished = finished;
-    }
+
     public void collision() {
         for(int i = 0; i < players.size()-1; i++) {
             if (planetID[i] != -1){
                 if (planetID[i] == planetID[i + 1]) {
                     if (players.get(i).getAngle() > players.get(i + 1).getAngle() - playerbounds && players.get(i).getAngle() < players.get(i + 1).getAngle() + playerbounds) {
-                        if (players.get(i).getAcceleration() > players.get(i + 1).getAcceleration()) {
+                        if (players.get(i).getRadAcceleration() > players.get(i + 1).getRadAcceleration()) {
                             players.remove(i + 1);
+
                             planetID[i+1] = -1;
                             System.out.println("player " + (i + 2) + " was killed");
                         } else {
@@ -70,6 +66,7 @@ public class Game extends JPanel {
             }
         }
     }
+
     public void addPlayers(JFrame frame){
         for(int i = 0; i < 4; i++) {
             randomSpawn(i);
@@ -80,10 +77,12 @@ public class Game extends JPanel {
             players.get(i).addKeyListener(frame); //Enables use of Keyboard inputs for players
         }
     }
+
     public void randomSpawn(int i){ //Random a planet for player to spawn on
         planetID[i] = (int)(Math.random()*planets.size());
         //System.out.println((int)(Math.random()*planets.size()));     //printing out values in menu screen, Investigate this
     }
+
     public void setPlayerKeys(){
         //Player1
         keys[0][0] = KeyEvent.VK_A; //Move
@@ -98,14 +97,25 @@ public class Game extends JPanel {
         keys[3][0] = KeyEvent.VK_C; //Move
         keys[3][1] = KeyEvent.VK_V; //Jump
     }
+
     public void generatePlanets(){
         planets.add(new Planet(56, 74, 100, 6, ResourceLoader.loadImage("planets/orangeplanet.png")));
         planets.add(new Planet(525, 356, 50, 20, ResourceLoader.loadImage("planets/testpurpleplanet.png")));
         planets.add(new Planet(550, 88, 75, 10, ResourceLoader.loadImage("planets/greenplanet.png")));
         planets.add(new Planet(250, 350, 50,14, ResourceLoader.loadImage("planets/redplanet.png")));
     }
+
     public void Winner(int playerID){
         System.out.println("Winner is " + playerID); //Do winning stuff here
         finished = true;
+    }
+
+    // Getters and Setters
+    public boolean getFinished(){
+        return finished;
+    }
+
+    public void setFinished(boolean finished){
+        this.finished = finished;
     }
 }
