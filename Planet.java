c1882e111f8d0bd276b77bc31fc1e2747cbba712
Jordan <jordanscarrott@gmail.com>
2016-12-08 14:38:36 +0200

    Added Movement constants
    MAX_VELOCITY and MAX_ACCELERATION (max acceleration per frame)

--- Planet.java
+++ Planet.java
@@ -4,6 +4,16 @@ import java.awt.image.BufferedImage;
 
 public class Planet extends JPanel {
 
+    /**
+     * Maximum velocity of a player on this planet
+     * */
+    public static final float MAX_VELOCITY = 1;
+
+    /**
+     * Maximum acceleration that can be applied to a player each frame
+     * */
+    public static final float MAX_ACCELERATION = 2;
+
     private BufferedImage imgPlanet = null;
     private MyVector pLocation;
     private MyVector center;
@@ -23,7 +33,7 @@ public class Planet extends JPanel {
         center = new MyVector(imgPlanet.getWidth() / 2, imgPlanet.getHeight() / 2);
         perimeter = 2 * (float)Math.PI * this.radius;
         // Scale the player's acceleration inversely to the planet's radius
-        planetaryAcceleration = 5 * (1 - this.radius / (PlanetGenerator.MAX_RADIUS + 10));
+        planetaryAcceleration = MAX_ACCELERATION * (1 - this.radius / (PlanetGenerator.MAX_RADIUS + 10));
     }
 
     public Planet(float x, float y, float radius, int bounds, BufferedImage imgPlanet) {
@@ -33,6 +43,16 @@ public class Planet extends JPanel {
     public void paint(Graphics g) {
         super.paint(g);
         g.drawImage(imgPlanet, (int) pLocation.x, (int) pLocation.y, null);
+    }
+
+    /**
+     * Display useful information about the Planet object represented by this instance
+     * */
+    public String toString() {
+        return "pLocation: " + pLocation
+                + "\tRadius: " + radius
+                + "\tPerimeter: " + perimeter
+                + "\tPlanetaryAcceleration: " + planetaryAcceleration;
     }
 
     // Getters and Setters
