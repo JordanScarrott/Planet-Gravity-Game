48e9b5f4c36e1d85fe2440e2406ebf8a8a62f7e4
Jordan <jordanscarrott@gmail.com>
2016-12-10 18:54:12 +0200

    Rearranged methods

--- Planet.java
+++ Planet.java
@@ -4,16 +4,14 @@ import java.awt.image.BufferedImage;
 
 public class Planet extends JPanel {
 
+    public static final float MAX_VELOCITY = 20;
+    public static final float MAX_ACCELERATION = 0.02f;
     private BufferedImage imgPlanet = null;
     private MyVector pLocation;
     private float radius;
     private float perimeter;
-
     private int spriteGlowX;
     private int spriteGlowY;
-
-    public static final float MAX_VELOCITY = 20;
-    public static final float MAX_ACCELERATION = 0.02f;
     private float thisPlanetsMax;
 
     private float planetaryAcceleration;
@@ -23,19 +21,25 @@ public class Planet extends JPanel {
         this.pLocation = location;
         this.radius = radius;
         this.imgPlanet = imgPlanet;
-        perimeter = 2 * (float)Math.PI * this.radius;
+        perimeter = 2 * (float) Math.PI * this.radius;
         this.spriteGlowX = spriteGlowX;
         this.spriteGlowY = spriteGlowY;
 //        planetaryAcceleration = Convert.map(radius, 110, 40, 0, MAX_ACCELERATION);
-        if(radius == 41) planetaryAcceleration = 0.05f;
+        if (radius == 41) planetaryAcceleration = 0.05f;
         else planetaryAcceleration = MAX_ACCELERATION * (1 - (this.radius / (175 + 10)));
-        if(radius == 100)thisPlanetsMax = 3;
-        if(radius == 75)thisPlanetsMax = 4;
-        if(radius == 50)thisPlanetsMax = 7;
-        if(radius == 41)thisPlanetsMax = 30;
+        if (radius == 100) thisPlanetsMax = 3;
+        if (radius == 75) thisPlanetsMax = 4;
+        if (radius == 50) thisPlanetsMax = 7;
+        if (radius == 41) thisPlanetsMax = 30;
     }
+
     public Planet(float x, float y, float radius, int spriteGlowX, int spriteGlowY, BufferedImage imgPlanet) {
         this(new MyVector(x, y), radius, spriteGlowX, spriteGlowY, imgPlanet);
+    }
+
+    public void paint(Graphics g) {
+        super.paint(g);
+        g.drawImage(imgPlanet, (int) pLocation.x - (int) radius - spriteGlowX, (int) pLocation.y - (int) radius - spriteGlowY, null);
     }
 
 
@@ -51,19 +55,21 @@ public class Planet extends JPanel {
     public float getRadius() {
         return radius;
     }
-    public float getThisPlanetsMax(){
+
+    public void setRadius(float radius) {
+        this.radius = radius;
+    }
+
+    public float getThisPlanetsMax() {
         return thisPlanetsMax;
     }
+
     public float getPlanetaryAcceleration() {
         return planetaryAcceleration;
     }
 
     public float getPerimeter() {
         return perimeter;
-    }
-
-    public void setRadius(float radius) {
-        this.radius = radius;
     }
 
     public BufferedImage getImgPlanet() {
@@ -73,10 +79,4 @@ public class Planet extends JPanel {
     public void setImgPlanet(BufferedImage imgPlanet) {
         this.imgPlanet = imgPlanet;
     }
-
-    public void paint(Graphics g) {
-        super.paint(g);
-        g.drawImage(imgPlanet, (int) pLocation.x - (int)radius -spriteGlowX, (int) pLocation.y - (int)radius -spriteGlowY, null);
-    }
-
 }
